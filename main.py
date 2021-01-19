1ea47f22e6f5005ab0a1dcc1086eee8d6bc5e02b
AlexanderSeryoginUpwork <alexander.seryogin.upwork@gmail.com>
2021-01-19 11:52:20 +0200

    Init commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import telebot
+
+bot = telebot.TeleBot('1557824608:AAGr3XUgn-yICz7k1tCC0w2y98x_GXI8Bn8')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
+keyboard1.row('Привет', 'Пока')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+
+bot.polling()
+
+
